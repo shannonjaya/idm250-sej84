8e242418b0aff50ae0aa173a42bc0e1a66b49f04
Shannon Jaya <183021335+shannonjaya@users.noreply.github.com>
2026-02-08 13:35:06 -0500

    Check if request method is POST before deleting

--- sku/delete-sku.php
+++ sku/delete-sku.php
@@ -2,7 +2,7 @@
  require_once './lib/sku.php';
 
 $id = isset($_POST['id']) ? intval($_POST['id']) : 0;
-if ($id) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $id) {
     delete_sku($connection, $id);
 }
 header("Location: ../idm250-sir/index.php?view=sku");
