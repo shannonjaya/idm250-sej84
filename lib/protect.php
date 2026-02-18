1e205421d18a377ecf36dff610b44152a9b227d3
Shannon Jaya <183021335+shannonjaya@users.noreply.github.com>
2026-02-18 14:00:03 -0500

    Fix user authentication path

--- lib/protect.php
+++ lib/protect.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . '/auth.php';
 
 if (!is_logged_in()) {
-  header('Location: ' . dirname($_SERVER['PHP_SELF'], 2) . '/user/login.php');
+  header('Location: ../idm250-sir/user/login.php');
   exit;
 }
